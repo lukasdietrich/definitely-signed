c626f54ecb80cb4362748df7e78d6daab289ed3a
Lukas Dietrich <lukas@lukasdietrich.com>
2025-04-01 21:43:58 +0200

    fix: Ignore encryption

--- src/lib/export.ts
+++ src/lib/export.ts
@@ -1,12 +1,12 @@
-import type {Dimensions} from 'src/lib/types';
-import {PDFDocument} from 'pdf-lib';
+import type { Dimensions } from 'src/lib/types';
+import { PDFDocument } from 'pdf-lib';
 
 export async function saveWithSignature(pdfOriginal: ArrayBuffer, signatureUrl: string, pageNumber: number, dimension: Dimensions) {
-	const doc = await PDFDocument.load(pdfOriginal);
+	const doc = await PDFDocument.load(pdfOriginal, { ignoreEncryption: true });
 	const page = doc.getPage(pageNumber - 1);
 	const signature = await doc.embedPng(signatureUrl);
-	
-	const {x, y, w, h} = dimension;
+
+	const { x, y, w, h } = dimension;
 	page.drawImage(signature, {
 		x,
 		y,
